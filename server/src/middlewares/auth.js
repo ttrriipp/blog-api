27e0e226ca3e8207d827529aac6afb3058e22167
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2026-06-28 12:30:48 +0800

    feat: add auth session check for auth middleware

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -1,5 +1,9 @@
 import passport from "passport";
 
 export const isAuthenticated = [
+  (req, res, next) => {
+    if (!req.user) return res.sendStatus(401);
+    next();
+  },
   passport.authenticate("jwt", { session: false }),
 ];
