4096a1c0cc4deae781309a5b9762791fc7b7a0c2
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2026-06-28 16:22:37 +0800

    feat: comment post middleware

--- server/src/routes/comment.js
+++ server/src/routes/comment.js
@@ -3,10 +3,24 @@ import controller from "../controllers/comment.js";
 import validate from "../middlewares/validate.js";
 import { isAdmin } from "../middlewares/admin.js";
 import { isAuthenticated } from "../middlewares/auth.js";
+import { postExists } from "../middlewares/comment.js";
 
 const router = Router({ mergeParams: true });
 
-router.get("/", controller.index);
-router.post("/create", isAuthenticated, validate.comment, controller.create);
+router.get("/", postExists, controller.index);
+router.post(
+  "/create",
+  isAuthenticated,
+  postExists,
+  validate.comment,
+  controller.create,
+);
+router.patch(
+  "/:commentId",
+  isAuthenticated,
+  postExists,
+  validate.comment,
+  controller.update,
+);
 
 export default router;
